4630aa3472a11353211113f103da497c75c0ce67
nv7z2 <barankacper2000@gmail.com>
2023-05-25 10:37:35 +0200

    feat: frontend init

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="login-view">
+    <h1>Login</h1>
+    <form class="login-form" @submit.prevent>
+      <input type="text" placeholder="Username" class="form-field">
+      <input type="password" placeholder="Password" class="form-field">
+      <input type="submit" value="Login to your account 🚀" class="form-button" />
+    </form>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+* {
+  box-sizing: border-box;
+}
+
+.login-view {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+.login-form {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 1.5rem;
+}
+
+.form-field {
+  width: 20rem;
+  padding: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+}
+
+.form-button {
+  width: 20rem;
+  padding: 1rem;
+  border: none;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+}
+</style>
